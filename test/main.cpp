de7d2c96c5e3a521bf29e1ed80674ebfa89a9a9f
fengweiqi <fengweiqi@sjtu.edu.cn>
2017-11-16 10:44:07 +0800

    2017.11.16

--- test/main.cpp
+++ test/main.cpp
@@ -1,16 +1,19 @@
 #include<iostream>
 #include<cstdlib>
 #include<cstdio>
+#include<cstring>
+#include<cassert>
 using namespace std;
-int &f(int a[],int j);
 int main()
 {
-      int* const p=NULL;
-     *p++;
-    //cout<<x<<" "<<y;
+char *s[3]={"a","vv","cc"};
+char **p;
+int *p1;
+int x=15;
+p1=&x;
+(*p1)++;
+cout<<*p1;
 
-}
-int &f(int a[],int j)
-{
-    return a[j];
+for(p=s;p<s+3;p++)
+cout<<**p<<endl;
 }
